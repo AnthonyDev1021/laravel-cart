706011ecabffd0f8e6e3294ee77bde7a54dce66a
unknown <AbthonyDev1021@gmail.com>
2023-08-08 09:50:21 -0400

    first commit

---
+++ src/facades/CartFacade.php
@@ -0,0 +1,10 @@
+<?php 
+namespace Devbuddy\Cart\Facades;
+use Illuminate\Support\Facades\Facade;
+
+class CartFacade extends Facade {
+    protected static function getFacadeAccessor()
+    {
+        return 'cart';
+    }
+}
